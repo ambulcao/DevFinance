e978e68948a18ac83a96e95f42f3ebb4a0184665
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-03-29 23:35:52 +0000

    Criando o layout da aplicacao

--- src/App.js
+++ src/App.js
@@ -10,47 +10,62 @@ function App() {
                     </title>
                 </head>
                 <body>
-            <h1>Dev-Financ€</h1>
+                    <header>
+                        <h1 id="logo">Dev-Financ</h1>
+                    </header>
 
-            <h2>Balanço</h2>
-            <h3>Entradas</h3>
-            <p>5.000,00 €</p>
-            
-            <h3>Saídas</h3>
-            <p>2.000,00 €</p>
+                    <main>
+                        <section id="balance">
+                            <h2>Balanço</h2>
 
-            <h3>Total</h3>
-            <p>3.000,00 €</p>
-            
-            <h2>Transações</h2>
+                            <div className="card">
+                                <h3>Entradas</h3>
+                                <p>5.000,00 €</p>
+                            </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Descrição</th>
-                        <th>Valor</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Luz</td>
-                        <td>- 500,00 €</td>
-                        <td>23/01/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Criação de Website</td>
-                        <td>5000,00 €</td>
-                        <td>23/01/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Internet</td>
-                        <td>- 200,00 €</td>
-                        <td>23/01/2021</td>
-                    </tr>    
-                </tbody>
-            </table>
-                <p>Dev.Financ€</p>
+                            <div className="card">
+                                <h3>Saídas</h3>
+                                <p>2.000,00 €</p>
+                            </div>
+
+                            <div className="card">
+                                <h3>Total</h3>
+                                <p>3.000,00 €</p>
+                            </div>
+                        </section>
+
+                        <section id="transaction">
+                            <h2>Transações</h2>
+                        
+                            <table id="data-table">
+                            <thead>
+                                <tr>
+                                    <th>Descrição</th>
+                                    <th>Valor</th>
+                                    <th>Data</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Luz</td>
+                                    <td>- 500,00 €</td>
+                                    <td>23/01/2021</td>
+                                </tr>
+                                <tr>
+                                    <td>Criação de Website</td>
+                                    <td>5000,00 €</td>
+                                    <td>23/01/2021</td>
+                                </tr>
+                                <tr>
+                                    <td>Internet</td>
+                                    <td>- 200,00 €</td>
+                                    <td>23/01/2021</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </section>
+                    </main>
+                    <footer><p>Dev.Finance €</p></footer>
                 </body>
             </html>
         </>
